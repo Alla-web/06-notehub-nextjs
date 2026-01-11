9ed33ba9d00182d57f0db2e2e7cf75c18dc27644
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-01-11 17:32:57 +0100

    fixed erroBox

--- components/ErrorBox/ErrorBox.tsx
+++ components/ErrorBox/ErrorBox.tsx
@@ -8,13 +8,13 @@ interface ErrorBoxProps {
 export default function ErrorBox({ query, errorMessage }: ErrorBoxProps) {
   return (
     <div className={css.errorBox}>
-      <p>Could not fetch the list of notes. {errorMessage}</p>
+      <p>Could not fetch the list of notes.</p>
+      {errorMessage && <p className={css.errorMessage}>{errorMessage}</p>}
       {query && (
         <p
           className={css.errorMessage}
         >{`No notes found with guwry: ${query}`}</p>
       )}
-      {errorMessage && <p className={css.errorMessage}>{errorMessage}</p>}
     </div>
   );
 }
